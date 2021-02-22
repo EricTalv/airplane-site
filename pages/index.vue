0e354ac10225869cbd753812216c5f13cf450924
EricTalv <eric.talviste@tptlive.ee>
2021-02-23 01:52:47 +0200

    added image

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="container-fluid p-0 m-0">
     <Navbar></Navbar>
+    <div class="row">
+      <div class="col-6">
+        <h1 class="title">Get your Airship Today</h1>
+        <h5 class="subtitle text-muted">We provide you with state of the art crafts.</h5>
+      </div>
+      <div class="col-6">
+        img
+      </div>
+    </div>
   </div>
 </template>
 
