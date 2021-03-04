d2e5a03c8251231e5ddb1710297002be53c2bf70
erictalv <eric.talviste@tptlive.ee>
2021-03-04 16:32:22 +0200

    fixing text and svgs in about section

--- pages/index.vue
+++ pages/index.vue
@@ -13,39 +13,35 @@
     <section class="about-section">
       <div class="row p-5 ">
 
-        <div class="col-md-6 col-sm-12 d-flex justify-content-center align-items-center">
+        <div class="col-md-6 col-sm-12">
           <img src="./../assets/images/what-we-do.svg" alt="">
         </div>
 
-        <div class="col-md-6 col-sm-12">
-          <div class="about-title">
-            <h2>What we do</h2>
-          </div>
-          <div class="about-p w-50">
+        <div class="col-md-6 col-sm-12 about-text-center">
+          <h2 class="about-title">What we do</h2>
+          <p class="about-p text-muted">
             We enterprise level flight services and solutions for your business.
             Starting from simple pilot and crew recruitment to having your flying business lift off the ground.
-          </div>
+          </p>
         </div>
 
       </div>
 
       <div class="row about-second p-5 ">
 
-        <div class="col-md-6 col-sm-12">
-          <div class="about-title">
-            <h2>Our background</h2>
-          </div>
-          <div class="about-p w-50">
+        <div class="col-md-6 col-sm-12 about-text-center">
+          <h2 class="about-title">Our background</h2>
+          <p class="about-p text-muted">
             Since 1985 we have build and curated small businesses with state of the art
             solutions tailored to their needs and wishes.
             <br>
             <br>
             We believe in four-dimensional management mobility,
             our upgraded model now offers 21st Century logistical paradigm shifts.
-          </div>
+          </p>
         </div>
 
-        <div class="col-md-6 col-sm-12 d-flex justify-content-center align-items-center">
+        <div class="col-md-6 col-sm-12 ">
           <img src="./../assets/images/our-background.svg" alt="">
         </div>
 
@@ -57,15 +53,13 @@
           <img src="./../assets/images/the-team.svg" alt="">
         </div>
 
-        <div class="col-md-6 col-sm-12 d-flex justify-content-center align-items-center flex-column">
-            <div class="about-title w-50">
-              <h2>The team</h2>
-            </div>
-            <div class="about-p w-50">
-              Our Team is comprised of 10 top level fight experts and enthusiasts.
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, consequuntur doloribus est provident
-              qui voluptatibus?
-            </div>
+        <div class="col-md-6 col-sm-12 about-text-center">
+          <h2 class="about-title ">The team</h2>
+          <p class="about-p text-muted">
+            Our Team is comprised of 10 top level fight experts and enthusiasts.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, consequuntur doloribus est provident
+            qui voluptatibus?
+          </p>
         </div>
 
       </div>
