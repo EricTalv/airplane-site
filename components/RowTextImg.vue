437b3cd3c357e297b9fd9201b4c422a8c1f74af4
erictalv <eric.talviste@tptlive.ee>
2021-03-09 22:00:26 +0200

    adding some minor fixes

--- components/RowTextImg.vue
+++ components/RowTextImg.vue
@@ -1,33 +1,35 @@
 <template>
 
   <div
-    class="row p-5"
-    :class="{
-                 'flex-column-reverse flex-md-row': reorder,
-           } ">
+    class="row p-5 flex-column-reverse"
+    :class="[ reorder ? 'flex-md-row' : 'flex-sm-row' ]
+
+    ">
     <div class="col-md-6 col-sm-12 about-img-wrapper">
       <img :src="require(`~/assets/images/${image}`)" alt="">
     </div>
 
     <div class="col-md-6 col-sm-12 about-text-center">
       <h2 class="about-title ">{{ title }}</h2>
-      <p class="about-p text-muted">
-        {{ description }}
+      <p class="about-p text-muted" v-html="description">
       </p>
     </div>
 
   </div>
 
 </template>
+
+<!--
+ If True - Images will be below on SM screen and on the RIGHT of text on MD screen
+-->
+
 <script>
 export default {
-  props: ['title', 'description', 'reorder', 'image'],
-  data() {
-    return {}
+  props: {
+    title: String,
+    description: String,
+    image: String,
+    reorder: Boolean,
   },
-
-  created() {
-    console.log(this.reorder)
-  }
 }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -11,68 +11,38 @@
 
     <!-- about -->
     <section class="about-section">
-      <div class="row p-5 flex-column-reverse flex-md-row">
-
-        <div class="col-md-6 col-sm-12 about-img-wrapper">
-          <img src="~/assets/images/what-we-do.svg" alt="">
-        </div>
-
-        <div class="col-md-6 col-sm-12 about-text-center">
-          <h2 class="about-title">What we do</h2>
-          <p class="about-p text-muted">
-            We enterprise level flight services and solutions for your business.
-            Starting from simple pilot and crew recruitment to having your flying business lift off the ground.
-          </p>
-        </div>
-
-      </div>
 
       <row-text-img
         title="What we do"
         description="We enterprise level flight services and solutions for your business.
             Starting from simple pilot and crew recruitment to having your flying business lift off the ground."
         image="what-we-do.svg"
-
-
+        reorder
 
       ></row-text-img>
 
-      <div class="row about-second p-5 ">
-
-        <div class="col-md-6 col-sm-12 about-text-center">
-          <h2 class="about-title">Our background</h2>
-          <p class="about-p text-muted">
-            Since 1985 we have build and curated small businesses with state of the art
+      <row-text-img
+        title="Our background"
+        description=" Since 1985 we have build and curated small businesses with state of the art
             solutions tailored to their needs and wishes.
             <br>
             <br>
             We believe in four-dimensional management mobility,
-            our upgraded model now offers 21st Century logistical paradigm shifts.
-          </p>
-        </div>
+            our upgraded model now offers 21st Century logistical paradigm shifts."
+        image="our-background.svg"
 
-        <div class="col-md-6 col-sm-12 about-img-wrapper ">
-          <img src="./../assets/images/our-background.svg" alt="">
-        </div>
+      ></row-text-img>
 
-      </div>
-
-      <div class="row about p-5 flex-column-reverse flex-md-row">
-
-        <div class="col-md-6 col-sm-12 about-img-wrapper">
-          <img src="./../assets/images/the-team.svg" alt="">
-        </div>
-
-        <div class="col-md-6 col-sm-12 about-text-center">
-          <h2 class="about-title ">The team</h2>
-          <p class="about-p text-muted">
-            Our Team is comprised of 10 top level fight experts and enthusiasts.
+      <row-text-img
+        title="The team"
+        description="Our Team is comprised of 10 top level fight experts and enthusiasts.
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, consequuntur doloribus est provident
-            qui voluptatibus?
-          </p>
-        </div>
+            qui voluptatibus?"
+        image="the-team.svg"
+        reorder
 
-      </div>
+      ></row-text-img>
+
     </section>
     <!-- about end -->
 
